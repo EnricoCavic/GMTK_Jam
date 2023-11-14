dc73423d88acc6c137086e2e2caac2cf540664d9
Enrico Cavicchioli <enricocavic@gmail.com>
2023-11-13 21:37:43 -0300

    Alterações de teste

--- Assets/Scripts/Gameplay/MovableObject.cs
+++ Assets/Scripts/Gameplay/MovableObject.cs
@@ -1,8 +1,5 @@
 using DPA.Managers;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class MovableObject : MonoBehaviour
