8aca87e70ef6aa45b58c90fb0169706d3dd3ba71
Enrico Cavicchioli <enricocavic@gmail.com>
2023-12-08 11:22:41 -0300

    Alterações de debug do bot
    Todas as funções de visualização de hitboxes e raycasts foram migradas para a função de OnDrawGizmos

--- Assets/Scripts/Gameplay/Bot/BotNavigator.cs
+++ Assets/Scripts/Gameplay/Bot/BotNavigator.cs
@@ -22,11 +22,13 @@ namespace DPA.Gameplay
         [Header("Collisions")] 
         [SerializeField] Vector2 frontBoxSize;
         [SerializeField] Vector2 groundBoxSize;
-        [SerializeField] Vector2 jumpOverBoxSize;
-        [SerializeField] Vector2 jumpOverOff;
+        [Space, SerializeField] float holeCheckDirection;
+        [SerializeField] float holeCheckDistance;
+        [Space, SerializeField] Vector2 jumpOverBoxSize;
+        [SerializeField] Vector2 jumpOverPosition;
 
         Vector2 GroundBox => new(hitBox.bounds.size.x * groundBoxSize.x, groundBoxSize.y);
-        
+        Vector2 HoleCheckDir => new(sideSwitch, holeCheckDirection);
 
         public BotWalk botWalk;
         public BotJump botJump;
@@ -43,8 +45,6 @@ namespace DPA.Gameplay
 
         void Start()
         {
-            //groundBoxSize = new Vector2(hitBox.bounds.size.x * 0.9f, 0.02f);
-
             InitializeDefaultState(botWalk);
 
             pauseHandler = ResumePauseHandler.Instance;
@@ -64,14 +64,20 @@ namespace DPA.Gameplay
 
         #region Detection
 
-        Vector2 topPosition => transform.position + new Vector3(-sideSwitch * hitBox.bounds.extents.x, hitBox.bounds.extents.y * 2.5f, 0);
+        Vector2 topPosition => transform.position + new Vector3(0f, hitBox.bounds.extents.y * 2.5f);
         Vector2 frontPosition => hitBox.bounds.center + new Vector3(hitBox.bounds.extents.x * sideSwitch, 0);
         public bool IsFalling => rb.velocity.y < 0.1f;
 
         public bool CanJumpOverWall()
         {
-            var topBlockHit = Physics2D.BoxCast(new Vector2(jumpOverOff.x * sideSwitch, jumpOverOff.y) + topPosition, jumpOverBoxSize, 0f, Vector2.right * sideSwitch, 0f, collisionMask);
-            return topBlockHit.collider == null;
+            return Physics2D.BoxCast(
+                new Vector2(jumpOverPosition.x * sideSwitch, jumpOverPosition.y) + topPosition,
+                jumpOverBoxSize,
+                0f,
+                Vector2.right * sideSwitch,
+                0f,
+                collisionMask
+                ).collider == null;
         }
 
         public bool IsGrounded()
@@ -83,14 +89,12 @@ namespace DPA.Gameplay
 
         public bool IsNearWall()
         {  
-            return Physics2D.BoxCast(frontPosition, frontBoxSize, 0, transform.right * sideSwitch, 0f, collisionMask).collider != null;
+            return Physics2D.BoxCast(frontPosition, frontBoxSize, 0f, transform.right * sideSwitch, 0f, collisionMask).collider != null;
         }
 
         public bool IsNearHole()
         {
-            var bottomHit = Physics2D.Raycast(topPosition, new Vector2(sideSwitch, -2f), 2f, collisionMask);
-            Debug.DrawRay(topPosition, new Vector2(sideSwitch, -2f), Color.red);
-            return bottomHit.collider == null;
+            return Physics2D.Raycast(topPosition, HoleCheckDir.normalized, holeCheckDistance, collisionMask).collider == null;
         }
 
         #endregion
@@ -165,7 +169,14 @@ namespace DPA.Gameplay
                 color = Color.green;
 
             Gizmos.color = color;
-            Gizmos.DrawCube(new Vector2(jumpOverOff.x * sideSwitch, jumpOverOff.y) + topPosition, jumpOverBoxSize);
+            Gizmos.DrawCube(new Vector2(jumpOverPosition.x * sideSwitch, jumpOverPosition.y) + topPosition, jumpOverBoxSize);
+
+            color = Color.red;
+            if(IsNearHole())
+                color = Color.green;
+
+            Gizmos.color = color;
+            Gizmos.DrawLine(topPosition, HoleCheckDir.normalized * holeCheckDistance + topPosition);
 
         }
     }
